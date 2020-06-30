6b8700b12c7fc760f79a9aaac99fd508fea90e9a
Matheus Henrique Pereira Santos <matheus.santos@Matheuss-MacBook-Pro.local>
2020-06-30 20:38:48 -0300

    add controller

--- src/main/java/com/beachtw/backend/BackendApplication.java
+++ src/main/java/com/beachtw/backend/BackendApplication.java
@@ -2,6 +2,8 @@ package com.beachtw.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +11,11 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello Teste %s!", name);
+	}
+
 
 }
